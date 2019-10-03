5949bd5015573adaaf4cc18cba565cad93b8e4fb
Kian Hooshmand <kianhooshmand@berkeley.edu>
2019-10-03 00:39:40 -0700

    Added skeleton routes to be implemented

--- index.js
+++ index.js
@@ -20,6 +20,45 @@ app.listen(3000, () => {
     });
 });
 
+/**
+ * Creates a user on initial login; stores their credentials
+ * in our DB. 
+ */
+app.post('/createUser', (req, res) => {
+
+})
+
+/**
+ * Authenticates a username/pw combo for login.
+ */
+app.get('/authenticateUser', (req, res) => {
+
+})
+
+/**
+ * Allows a user to pay another user; user must specify
+ * recipient's username and payment amount.
+ */
+app.post('/pay', (req, res) => {
+    
+})
+
+/**
+ * Allows a user to create a new ledger account to their 
+ * list of stored accounts. 
+ */
+app.post('/addUserAccount', (req, res) => {
+    // adds a user account to a user
+})
+
+/**
+ * Allows a user to add a friend to their list of friends
+ * in our DB. 
+ */
+app.post('/addFriend', (req, res) => {
+    
+})
+
 app.post("/person", (request, response) => {
     collection.insertOne({ username: "ff" }, (error, result) => {
         if(error) {
